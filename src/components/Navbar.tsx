049dbe62645687350bb729e946b8697339708c0e
Mihir9702 <mkpatel9702@gmail.com>
2024-05-12 23:39:51 -0400

    automation software page complete

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,19 +2,17 @@ import Image from "next/image";
 import Link from "next/link";
 import logo from "../../public/logo.png";
 import { useState } from "react";
-import {
-  AutomationSolutionsList,
-  ProductServicesList,
-} from "@/helpers/ListOptions";
 import { cards, moreCards } from "@/helpers/getCards";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
   const [showAutomationSolutions, setShowAutomationSolutions] = useState(false);
   const [showProductServices, setShowProductServices] = useState(false);
 
+  const router = useRouter();
   return (
     <nav className="flex bg-gray-100 justify-around items-center py-12">
-      <Image src={logo} alt="logo" />
+      <Image src={logo} alt="logo" onClick={() => router.push("/")} />
       <ul className="flex justify-end gap-10 p-4 text-xl">
         <li className="hover:text-kpam-blue">
           <Link href="/about">About</Link>
@@ -35,14 +33,15 @@ export default function Navbar() {
                 text-[16px] max-w-[200px] text-center rounded-lg"
             >
               {cards.map((card, i) => (
-                <li
-                  key={card.title}
-                  className={`${
-                    i % 2 !== 0 ? "bg-gray-200" : ""
-                  } hover:bg-kpam-blue hover:text-white cursor-pointer`}
-                >
-                  <Link href={card.href}>{card.title}</Link>
-                </li>
+                <Link key={card.title} href={card.href}>
+                  <li
+                    className={`${
+                      i % 2 !== 0 ? "bg-gray-200" : ""
+                    } hover:bg-kpam-blue hover:text-white cursor-pointer`}
+                  >
+                    {card.title}
+                  </li>
+                </Link>
               ))}
             </ul>
           )}
@@ -63,14 +62,15 @@ export default function Navbar() {
                 text-[16px] max-w-[200px] text-center rounded-lg"
             >
               {moreCards.map((card, i) => (
-                <li
-                  key={card.title}
-                  className={`${
-                    i % 2 !== 0 ? "bg-gray-200" : ""
-                  } hover:bg-kpam-blue hover:text-white cursor-pointer`}
-                >
-                  <Link href={card.href}>{card.title}</Link>
-                </li>
+                <Link key={card.title} href={card.href}>
+                  <li
+                    className={`${
+                      i % 2 !== 0 ? "bg-gray-200" : ""
+                    } hover:bg-kpam-blue hover:text-white cursor-pointer`}
+                  >
+                    {card.title}
+                  </li>
+                </Link>
               ))}
             </ul>
           )}
---
+++ src/pages/automation-software-solutions.tsx
@@ -0,0 +1,22 @@
+import Header from "@/components/Header";
+import Navbar from "@/components/Navbar";
+import autoSoftware from "@/../public/Automation Software.jpg";
+import Image from "next/image";
+import Footer from "@/components/Footer";
+import AutomationSoftwareText from "@/components/AutomationSoftwareText"
+
+export default function DigitalAutomationTransformation() {
+  return (
+    <main>
+      <Navbar />
+      <Header text="Automation Software and Solutions for PLC & HMI" />
+
+      <section className="bg-gray-100 flex items-start justify-center gap-8 p-8">
+        <AutomationSoftwareText />
+        <Image src={autoSoftware} alt="" className="" />
+      </section>
+
+      <Footer />
+    </main>
+  );
+}
